10f477ffc7e563b9861fd7928c2e10b1a730675f
keynd77 <keynd77@gmail.com>
2025-08-22 23:14:03 +0200

    cloud change

--- src/components/Clouds.tsx
+++ src/components/Clouds.tsx
@@ -1,49 +1,57 @@
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import { Clouds, Cloud } from '@react-three/drei'
+import { Clouds, Cloud, Float } from '@react-three/drei'
 
 const CloudScene = () => {
   useFrame(({ camera }, delta) => {
-    camera.position.z += delta * 10 // fly forward endlessly
+    camera.position.z += delta * 1 // fly forward endlessly
   })
 
   return (
     <group position={[0, 0, 0]}>
       <Clouds material={THREE.MeshLambertMaterial} limit={400} range={150}>
-        <Cloud 
-          seed={2}
-          segments={20}
-          volume={6}
-          opacity={0.8}
-          fade={10}
-          growth={4}
-          speed={0.05}
-          bounds={[6, 1, 1]}
-          color="white"
-          position={[15, 3, 0]}
-        />
-        <Cloud 
-          seed={3}
-          segments={20}
-          volume={6}
-          opacity={0.8}
-          fade={10}
-          growth={4}
-          speed={0.05}
-          bounds={[6, 1, 1]}
-          color="white"
-          position={[-15, 3, 0]}
-        />
-        <Cloud 
-          concentrate="outside"
-          growth={50}
-          color="white"
-          opacity={1.0}
-          seed={0.3}
-          bounds={100}
-          volume={100}
-          position={[0, 10, 0]}
-        />
+        <Float speed={1} rotationIntensity={0.3} floatIntensity={0.5}>
+          <Cloud 
+            seed={2}
+            segments={20}
+            volume={6}
+            opacity={0.8}
+            fade={10}
+            growth={4}
+            speed={0.05}
+            bounds={[6, 1, 1]}
+            color="white"
+            position={[15, 3, 0]}
+          />
+        </Float>
+
+        <Float speed={1} rotationIntensity={0.3} floatIntensity={0.5}>
+          <Cloud 
+            seed={3}
+            segments={20}
+            volume={6}
+            opacity={0.8}
+            fade={10}
+            growth={4}
+            speed={0.05}
+            bounds={[6, 1, 1]}
+            color="white"
+            position={[-15, 3, 0]}
+          />
+        </Float>
+
+        <Float speed={1} rotationIntensity={0.2} floatIntensity={0.4}>
+          <Cloud 
+            concentrate="outside"
+            growth={50}
+            color="white"
+            opacity={1.0}
+            seed={0.3}
+            bounds={100}
+            volume={100}
+            position={[0, 10, 0]}
+          />
+        </Float>
       </Clouds>
     </group>
   )
